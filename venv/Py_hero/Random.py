31f995bc593671b67dc35466e582682d364b77cf
Amankhalsa <amansin31@gmail.com>
2021-06-17 16:48:41 +0530

    Random Number (OTP)

---
+++ venv/Py_hero/Random.py
@@ -0,0 +1,12 @@
+# Built In Methods
+# Importing Modules
+# import --
+# import --as --
+#  from --- import ---
+# can use this below
+# import random
+
+from random import randint
+
+# used for otp genration
+print("One time OTP is ;",randint(35000, 150000))
