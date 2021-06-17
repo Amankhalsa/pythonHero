09172f5ea6ffd6273a02cb5a71602d1f99caa60f
Amankhalsa <amansin31@gmail.com>
2021-06-17 19:30:36 +0530

    Guess number game

--- venv/Py_hero/if_else.py
+++ venv/Py_hero/if_else.py
@@ -1,3 +1,17 @@
+print("if else ")
+"""
+Python Conditions and If statements
+Python supports the usual logical conditions from mathematics:
+
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+"""
+
+
 passing_marks = 35
 obtained_marks = int(input('Plese enter your Obtained marks: '))
 
@@ -17,3 +31,16 @@ elif obtained_marks > passing_marks and  obtained_marks <=40:
     print(f'your Marks is {obtained_marks} and passed ')
 else:
     print('you failed')
+
+
+# Short Hand If ... Else
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+
+# Short Hand If ... Else 2
+a = 330
+b = 330
+
+print("A") if a > b else print("=") if a == b else print("B")
