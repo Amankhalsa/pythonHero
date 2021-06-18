e582da626020bea51f52ce0b30a13b889ae5676e
Amankhalsa <amansin31@gmail.com>
2021-06-18 14:57:43 +0530

    Random game

---
+++ venv/Py_hero/Random_game.py
@@ -0,0 +1,9 @@
+import  random
+
+color= ["red", "grwwn", "blue","yellow"]
+
+body=["Right hand", "left hand", "Right foot", "left foot"]
+
+test1= random.choice(color)
+test2=random.choice(body)
+print(test1,test2)
---
+++ venv/Py_hero/list_in_py.py
@@ -0,0 +1,10 @@
+# list
+
+
+# All data types ko hold karti
+# Its Ordered
+test_list = [1, 1.2, 'String', [1, 2, 3], {'key': 'Aman'}, (1, 2)]
+print(test_list[3][0])
+print(test_list[4]["key"])
+print(test_list[5][1])
+print(test_list[3])
