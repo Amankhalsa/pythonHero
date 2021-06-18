53923acd62bdf757d76f393e13ec05f8c85196fd
Amankhalsa <amansin31@gmail.com>
2021-06-18 17:35:07 +0530

    set or tuple

--- venv/Py_hero/list_comprehen_in_py.py
+++ venv/Py_hero/list_comprehen_in_py.py
@@ -12,3 +12,17 @@ print(list_com)
 print('x' * 20)
 list_com = [x for x in range(30) if x % 2 == 1]
 print(list_com)
+
+"""
+The Syntax
+newlist = [expression for item in iterable if condition == True]
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+"""
---
+++ venv/Py_hero/set_tupple.py
@@ -0,0 +1,15 @@
+Tupels = ()
+Sets = {}
+
+list_test = [1, 2, 3, 4, 5, 6]
+list_test[2] = 130
+print(list_test)
+
+tuple_test = (12, 52, 3, 4, 5)
+# tuple_test[2] = 130
+print(tuple_test)
+test2={12, 52, 3, 4, 5}
+set_test = {'john', 'sally', 'gorege', 'ratan'}
+print(set_test)
+# test2.sort()
+print(55 in test2)
