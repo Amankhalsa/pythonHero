e38890ffafe99abafd5205df64d6771598f548c5
Amankhalsa <amansin31@gmail.com>
2021-06-18 15:18:56 +0530

    list and methods

---
+++ venv/Py_hero/list_method_&_fun.py
@@ -0,0 +1,56 @@
+# Most Used List Methods
+
+# append
+# remove
+# pop
+# sort
+# reverse
+# Most Used List Functions
+# len()
+# min()
+# max()
+# --------------------------------
+
+print("methods")
+"""
+Python has a set of built-in methods that you can use on lists/arrays.
+
+Method	    Description
+append()	Adds an element at the end of the list
+clear()	    Removes all the elements from the list
+copy()	    Returns a copy of the list
+count() 	Returns the number of elements with the specified value
+extend()	Add the elements of a list (or any iterable), to the end of the current list
+index()	    Returns the index of the first element with the specified value
+insert()	Adds an element at the specified position
+pop()	    Removes the element at the specified position
+remove()	Removes the first item with the specified value
+reverse()	Reverses the order of the list
+sort()	    Sorts the list
+"""
+
+
+
+list_int = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+list_char = ["apple" ,"bananna" , "mango","cherry", "ship"]
+
+# Function call
+# functionname()
+# Method
+# object.method(arg)
+# Function
+# functionname(object)
+# list_int.append(0)
+# list_int.pop(0)
+# list_int.remove(9)
+# list_int.sort()
+# list_char.sort()
+
+# print(len(list_char))
+#
+# print(len(list_int))
+
+# print(max(list_int))
+print(max(list_char))
+list_char.append("carrot")
+print(list_char)
