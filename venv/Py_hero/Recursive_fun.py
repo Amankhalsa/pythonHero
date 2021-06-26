33c09e7bb9d35270dbd8c75d1dc062b395026f9c
Amankhalsa <amansin31@gmail.com>
2021-06-26 18:07:58 +0530

    recursive by calling to fun

---
+++ venv/Py_hero/Recursive_fun.py
@@ -0,0 +1,21 @@
+# def fun_name(x,y):
+#  print(x *y)
+#  fun_name(x, y)
+#
+#
+#
+#
+# fun_name(2,3)
+
+
+def table(num, i):
+    result = num * i
+
+    print(f'{i} X {num} = {result}')
+    i += 1
+    while i != 11:
+        table(num, i)
+        break
+
+
+table(5, 1)
---
+++ venv/Py_hero/first_function.py
@@ -0,0 +1,16 @@
+# this function will print table
+def fun_name(y):
+    for a in range(1, 11):
+        print(f"{a} x {y} = {a * y}")
+    else:
+        print("Table is printed....")
+
+
+fun_name(12)
+
+#
+# def fun_name(x, y):
+#     print(x * y)
+#
+#
+# fun_name(2, 2)
