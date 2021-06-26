eac128f06a77a59826f2d2e6180d6ef351d16350
Amankhalsa <amansin31@gmail.com>
2021-06-26 14:33:19 +0530

    GBP ADDED 103

--- venv/Py_hero/cur_conv.py
+++ venv/Py_hero/cur_conv.py
@@ -4,7 +4,7 @@
 # Out Put Result in welformated Sentense
 
 
-conv_rates = {'USD': 74.23, 'GBP': 1.39, 'CAD': 60.39, 'CHF': 80.95, 'AUD': 56.32, 'INR': 1}
+conv_rates = {'USD': 74.23, 'GBP': 103.06, 'CAD': 60.39, 'CHF': 80.95, 'AUD': 56.32, 'INR': 1}
 
 usr_amt = int(input('Manu Note Wakha Mera Mode banay: '))
 usr_cur = input('Oye Kera Mall haiga tery Kol: ')
