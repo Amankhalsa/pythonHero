888e7f839d6480b1c929711898bdf0b14e8d70cb
Amankhalsa <amansin31@gmail.com>
2021-06-26 16:01:18 +0530

    my dic

---
+++ venv/Py_hero/while.py
@@ -0,0 +1,42 @@
+# for loop example
+# for x in range(6):
+#   print(x)
+# else:
+#   print("Finally finished!")
+
+# for i in range(10):
+#  print(i)
+
+#
+# i = 0
+# while i <10 :
+#  print('I am still True')
+#  i += 1
+
+i = 1
+while i < 11:
+  print(f"{i} x 2 = {i*2} ")
+  i += 1
+print(" x "*20)
+i = 1
+while i < 11:
+  print(f"{i} x 2 = {i*2} ")
+  if i == 3:
+    break
+  i += 1
+
+print("new-loop "*5)
+i = 1
+while i < 10:
+    i += 1
+    print(f"{i} x 2 = {i * 2} ")
+    if i == 3:j
+         continue
+
+print("next-example  "*3)
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
