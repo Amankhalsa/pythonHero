d539b91fa10ce5555e51760745f6f419e4ba621d
Amankhalsa <amansin31@gmail.com>
2021-06-27 18:29:46 +0530

    Dictonary

---
+++ venv/Py_hero/Request/Requ.py
@@ -0,0 +1,27 @@
+
+#Request
+#Http GET AND POST
+# .GET
+# .content
+# API
+
+import requests
+url = 'https://api.exchangerate-api.com/v4/latest/eur'
+test = requests.get(url)
+
+info = test.json()
+
+print(info['rates'])
+
+# url = 'https://www.youtube.com/'
+# test = requests.get(url)
+# info = test.content
+# print(info)
+
+# url="https://www.youtube.com/"
+# test=requests.get(url)
+#
+# info=test.content
+# print(test)
+# print(info)
+
