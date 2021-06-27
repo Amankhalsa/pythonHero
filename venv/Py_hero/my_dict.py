2e5b5b2e4a36fba1ee3e414ee7d483ea9d431d69
Amankhalsa <amansin31@gmail.com>
2021-06-27 18:36:24 +0530

    Eng to pbi

---
+++ venv/Py_hero/my_dict.py
@@ -0,0 +1,93 @@
+print("This is a dictionary Eng to Punjabi:")
+print("user for word meaning eng to pbi")
+my_dict={"Happy":"ਖੁਸ਼",
+	"Healthy":"ਤੰਦਰੁਸਤ",	
+	"Clever":"ਚਲਾਕ",
+	"Wise":	"ਸਮਝਦਾਰ",	
+	"Laborious":"ਮਿਹਨਤੀ",	
+	"Honest":"ਇਮਾਨਦਾਰ",	
+	"Brave":"ਬਹਾਦਰ",	
+	"Rich":"ਅਮੀਰ",	
+	"Kind":"ਦਯਾਲੁ",	
+	"Intelligent":"ਤੇਜ"}
+# user_input=input("enter your word:")
+# print("Answer is : ",my_dict[user_input])
+# print(my_dict.keys())
+# print(my_dict.values())
+# my_dict['key'] = 'value'
+
+# a= input("Enter key:")
+# b= input("Enter Value:")
+# my_dict[a]=b
+
+# print(my_dict.items())
+
+
+		#for loopp for access
+# for key in my_dict:
+# 	# print(key)
+# 	# print(my_dict[key])
+# 	print(key,"=", my_dict[key])
+		#for loopp for access
+# for key,value in my_dict.items():
+# 	print(key,"=", value)
+
+
+
+
+# 2nd example
+# dict_test = {'john': 25, 'sally': 28, 'george': 33}
+
+# print(dict_test['key3'])
+# dict_test['key1'] = 30
+# dict_test['key4'] = 'value4'
+# print(dict_test['george'])
+
+# print(dict_test)
+# for key in dict_test:
+#  print(dict_test[key])
+
+# for key,value in dict_test.items():
+#  print(key,value)
+# print(dict_test.pop('john'))
+
+
+# full detail
+"""
+Python Dictionary Methods
+Python has a set of built-in methods that you can use on dictionaries.
+
+Method	Description
+clear()			Removes all the elements from the dictionary
+copy()			Returns a copy of the dictionary
+fromkeys()		Returns a dictionary with the specified keys and value
+get()			Returns the value of the specified key
+items()			Returns a list containing a tuple for each key value pair
+keys()			Returns a list containing the dictionary's keys
+pop()			Removes the element with the specified key
+popitem()		Removes the last inserted key-value pair
+setdefault()	Returns the value of the specified key. 
+				If the key does not exist: insert the key, 
+				with the specified value
+update()		Updates the dictionary with the specified key-value pairs
+values()		Returns a list of all the values in the dictionary
+https://www.w3schools.com/python/python_ref_dictionary.asp
+https://www.w3schools.com/python/python_dictionaries.asp
+"""
+# x = ('Name', 'Class', 'Roll no')
+# y = input("enter value")
+#
+# thisdict = dict.fromkeys(x, y)
+#
+#
+# print(thisdict)
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+#
+# car.pop("model")
+#
+# print(car)
