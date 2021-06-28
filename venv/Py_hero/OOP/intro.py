59501eb673b9a14ea4d1122175e7ebcfbd7dc7f5
Amankhalsa <amansin31@gmail.com>
2021-06-28 12:43:10 +0530

    oop

---
+++ venv/Py_hero/OOP/intro.py
@@ -0,0 +1,81 @@
+# CODE
+print("oop concepts")
+# #
+
+# #
+
+# # Funcational Programming
+
+# #
+
+# # Variables
+
+# # Functions
+
+# #
+
+# #
+
+# #
+
+# #
+
+# #
+
+# # Object Oriented Programming
+
+# # class map:
+
+# # attribues
+
+# # Methods
+
+# #
+
+# #
+
+# # class acount
+
+# # name=
+
+# # addres=
+
+# # sdad =
+
+#
+
+#
+
+# class my_class:
+
+# x = 2
+
+# name = 'Khan'
+
+#
+
+# def fun_name():
+
+# print('Hello')
+
+#
+
+#
+
+#
+
+# obj1 = my_class
+
+#
+
+#
+
+#
+
+#
+
+#
+
+# obj4.fun_name()
+
+#
